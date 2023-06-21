88f2bf1045bb13a49e9c763d90be6b67ee75a36c
chilinhnguyen <nguyenlinhchitqp@gmail.com>
2023-06-21 09:24:07 +0700

    update spawn ground

--- Assets/Script/GroundSpawnerController.cs
+++ Assets/Script/GroundSpawnerController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -29,23 +29,26 @@ public class GroundSpawnerController : MonoBehaviour
     public void SpawnGround()
     {
         int randomNum = UnityEngine.Random.Range(1, 5);
-        if(randomNum == 1)
+        float groundOffset = 5f; // Khoảng cách giữa các ground
+
+        if (randomNum == 1)
         {
-            Instantiate(Ground1, new Vector3(transform.position.x + 4, -5.11f, 0), Quaternion.identity);
+            Instantiate(Ground1, new Vector3(transform.position.x + groundOffset, -4.66f, 0), Quaternion.identity);
         }
-        if (randomNum == 2)
+        else if (randomNum == 2)
         {
-            Instantiate(Ground2, new Vector3(transform.position.x + 4, -6.8f, 0), Quaternion.identity);
+            Instantiate(Ground2, new Vector3(transform.position.x + groundOffset * 1.5f, -8.2f, 0), Quaternion.identity);
         }
-        if (randomNum == 3)
+        else if (randomNum == 3)
         {
-            Instantiate(Ground3, new Vector3(transform.position.x + 4, -3.13f, 0), Quaternion.identity);
+            Instantiate(Ground3, new Vector3(transform.position.x + groundOffset, 6.12f, 0), Quaternion.identity);
         }
-        if (randomNum == 4)
+        else if (randomNum == 4)
         {
-            Instantiate(Ground4, new Vector3(transform.position.x + 5, -3.72f, 0), Quaternion.identity);
+            Instantiate(Ground4, new Vector3(transform.position.x + groundOffset * 1.2f, -4.55f, 0), Quaternion.identity);
         }
     }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
