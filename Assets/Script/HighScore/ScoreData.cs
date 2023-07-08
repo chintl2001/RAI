f40b02ec417ffa0cdd2c2c1aea9f15adb8d7e707
quynhbn272 <115777816+quynhbn27@users.noreply.github.com>
2023-07-09 03:35:07 +0700

    Add file highScore

--- Assets/Script/HighScore/ScoreData.cs
+++ Assets/Script/HighScore/ScoreData.cs
@@ -1,14 +1,5 @@
-/*using System.Collections.Generic;
-
 [System.Serializable]
 public class HighScoreData
 {
-    public List<HighScoreEntry> highScores = new List<HighScoreEntry>();
+    public int highScore;
 }
-
-[System.Serializable]
-public class HighScoreEntry
-{
-    public string playerName;
-    public int score;
-}*/
