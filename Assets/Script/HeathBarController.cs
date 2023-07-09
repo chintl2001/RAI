6cb4c79d3127e07ef27d7b21318d509e9c454929
chilinhnguyen <nguyenlinhchitqp@gmail.com>
2023-07-09 19:36:09 +0700

    load upgrade sence

--- Assets/Script/HeathBarController.cs
+++ Assets/Script/HeathBarController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class HeathBarController : MonoBehaviour
@@ -24,6 +25,10 @@ public class HeathBarController : MonoBehaviour
         if (health <= 0)
         {
             DestroyEnemy();
+        }
+        if(health == 0)
+        {
+            SceneManager.LoadScene("Upgrade");
         }
     }
 
